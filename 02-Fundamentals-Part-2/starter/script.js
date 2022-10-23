257e27b860ef2bf3d616f29c9a22a4218a157cb3
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-10-23 23:51:55 +0900

    strict mode functionality

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,9 @@
+// Strict mode 
+
+'use strict'; //highlights the errors and reserved words
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
