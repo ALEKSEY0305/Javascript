354aa9e3f8818e46197bdb9b3355427e682d53a7
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-10-30 22:17:09 +0900

    debugger

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -74,3 +74,47 @@ const temperatures = [
 // console.log(amplitudeNew);
 
 // expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+
+
+
+const measureKelvin = function() {
+    const measurement = {
+        type: 'temp',
+        unit: 'celsius',
+        // value: Number(prompt('Degrees celsius:')),
+        value: 10,
+    }
+
+
+    //find bug
+    console.log(measurement.value);
+    console.table(measurement);
+    // console.warn(measurement.value);
+    // console.error(measurement.value);
+    const kelvin = measurement.value + 273;
+    return kelvin;
+}
+console.log(measureKelvin())
+
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+
+    const temps = t1.concat(t2)
+    console.log(temps)
+
+    let max = temps[0];
+    let min = temps[0];
+        for (let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i];
+        if(typeof curTemp !== 'number') continue;
+        // debugger;
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+}
+const amplitudeBug = calcTempAmplitudeBug(temperatures)
+console.log(amplitudeBug);
