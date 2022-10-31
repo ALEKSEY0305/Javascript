00098ab4abd9f7590bd7123d2796eed10b0b8b86
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-01 02:11:16 +0900

    manipulating css

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -17,13 +17,21 @@ let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess)
+    // console.log(guess, typeof guess)
 
-
+    // When there's no input
     if(!guess) {
         document.querySelector('.message').textContent = '⛔️ No Number!';
+
+        // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉Correct Number!';
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        // when the guess is too high
     } else if ( guess > secretNumber) {
         if(score > 1) {
         document.querySelector('.message').textContent = '📈 Too High!';
@@ -33,6 +41,8 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent  = '💣You lost the game!';
         document.querySelector('.score').textContent = 0;
         } 
+
+        //when the guess is too low
     } else if ( guess < secretNumber) {
         if(score > 1) {
         document.querySelector('.message').textContent = '📉 Too low!';
