66e9d4d2bbd1f399088a052a7d76754d623f005e
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-04 03:20:30 +0900

    js behind the scenes

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,24 +14,23 @@ const bntHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 // Starting conditions
-const init = function() {
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-    scores = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
-    playing = true;
-
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    diceEl.classList.add('hidden'); 
-    player0El.classList.remove('player--winner');
-    player1El.classList.remove('player--winner');
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 };
- init();
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
