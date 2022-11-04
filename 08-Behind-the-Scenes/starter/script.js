a0db686fe3976dfb32aebe138a14ec63b02c0e8a
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-05 02:31:31 +0900

    this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,37 +1,37 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2022 - birthYear;
+// function calcAge(birthYear) {
+//     const age = 2022 - birthYear;
 
-    function printAge() {
-        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
+//     function printAge() {
+//         let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//         console.log(output);
         
-        if(birthYear >= 1981 && birthYear <= 1996) {
-            var millenial = true;
-            // creating NEW variable with the same name as outer scope's variable
-            const firstName = 'Steven';     // first name will change because JS first looks for a variable in the current block
+//         if(birthYear >= 1981 && birthYear <= 1996) {
+//             var millenial = true;
+//             // creating NEW variable with the same name as outer scope's variable
+//             const firstName = 'Steven';     // first name will change because JS first looks for a variable in the current block
 
-            // Reassigning outer scope's variable
-            output = 'NEW OUTPUT'
-            const str = `Oh, you are a millenial, ${firstName}`;
-            console.log(str);
+//             // Reassigning outer scope's variable
+//             output = 'NEW OUTPUT'
+//             const str = `Oh, you are a millenial, ${firstName}`;
+//             console.log(str);
 
-            function add(a, b) {
-                return a + b;
-            } console.log(add (2, 7)); // will work only in the func block
+//             function add(a, b) {
+//                 return a + b;
+//             } console.log(add (2, 7)); // will work only in the func block
 
-        }
-        console.log(millenial)   // var can be accessed because it's a function scope variable.
-        // console.log(str) cannot be accessed because it's a block scope (let too)
-        // add(2 + 3); doesn't work because funcs are blocks in ES6
-        console.log(output)
-    }
-    printAge();
-    return age;
-}
-const firstName = 'Alex';
-calcAge(1995);
+//         }
+//         console.log(millenial)   // var can be accessed because it's a function scope variable.
+//         // console.log(str) cannot be accessed because it's a block scope (let too)
+//         // add(2 + 3); doesn't work because funcs are blocks in ES6
+//         console.log(output)
+//     }
+//     printAge();
+//     return age;
+// }
+// const firstName = 'Alex';
+// calcAge(1995);
 
 
 
@@ -44,18 +44,16 @@ calcAge(1995);
 
 
 
-const numbers = [21, 221, 543234, 435,]
+// const numbers = [21, 221, 543234, 435,]
 
-const bigNum = function(arr) {
-    let max = arr[0];
-    for(let i = 0; i < arr.length; i++) {
-        const array = arr[i]
-        if(array > max) max = array
-    } console.log(max)
-}
-const check = bigNum(numbers)
-
-
+// const bigNum = function(arr) {
+//     let max = arr[0];
+//     for(let i = 0; i < arr.length; i++) {
+//         const array = arr[i]
+//         if(array > max) max = array
+//     } console.log(max)
+// }
+// const check = bigNum(numbers)
 
 
 
@@ -68,44 +66,95 @@ const check = bigNum(numbers)
 
 
 
-console.log(me);                   // Cannot access a variable before declaration.
-// console.log(job);                   // Dead zone
-// console.log(year);            
 
-var me = 'Alex';                    // will be hoisted but turns into UNDFINED
-let job = 'teacher';
-const year = 1991;
 
-// FUNCTIONS
-console.log(addDecl(2, 3))             // can Access
-// console.log(addExpr(2, 3))             // cannot Access because it was declared as CONST
-// console.log(addArrow(2, 3))            // will not work
+// console.log(me);                   // Cannot access a variable before declaration.
+// // console.log(job);                   // Dead zone
+// // console.log(year);            
 
-function addDecl(a, b) {
-    return a + b;
-}
+// var me = 'Alex';                    // will be hoisted but turns into UNDFINED
+// let job = 'teacher';
+// const year = 1991;
 
-const addExpr = function(a, b) {
-    return a + b;
-}
+// // FUNCTIONS
+// console.log(addDecl(2, 3))             // can Access
+// // console.log(addExpr(2, 3))             // cannot Access because it was declared as CONST
+// // console.log(addArrow(2, 3))            // will not work
 
-var addArrow = (a, b) => a + b;
+// function addDecl(a, b) {
+//     return a + b;
+// }
+
+// const addExpr = function(a, b) {
+//     return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
 
 
 
 
 //Example
-console.log(numProducts) ;             // will be undefined
-if(!numProducts) deleteShoppingCart();       //will work because var will be undefined
-var numProducts = 10;                // will be undefined
-function deleteShoppingCart() {
-    console.log('All products are deleted');
-}
+//     
 
-var x = 1;
-let y = 2;
-const z = 3;
 
-console.log(x === window.x) // VAR variable will create properties in Window
-console.log(y === window.y)
-console.log(z === window.z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS
+
+// console.log(this);
+
+const calcAge = function(birthYear) {
+    console.log(2022 - birthYear);
+    console.log(this);
+};
+calcAge(1995);
+
+
+const calcAgeArrow = birthYear => {
+    console.log(2022 - birthYear);
+    // console.log(this);
+};
+calcAgeArrow(1951);
+
+const alex = {
+    year: 1995,
+    calcAge: function () {
+        console.log(this);    //opens Window because it has no params
+        console.log(2022 - this.year);  // will work because param is attached to it
+    },
+};
+alex.calcAge();
+
+
+const matilda = {
+    year: 2017,
+};
+
+
+
+matilda.calcAge = alex.calcAge;     //metjod borrowing
+matilda.calcAge();                  // 'this' from 144 is pointed to Matilda.  This keyword is dynamic
+
+const f = alex.calcAge;
+f();                               //regular func  not attached to anything
