9235ac0a7488580b75ce201ceb6a813ee3a41752
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-05 23:29:18 +0900

    primitives and objects

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -242,18 +242,45 @@
 
 
 
-// Arguments keyword        //Not actual, but good to know
-const addExpr = function(a, b) {
-    console.log(arguments)
-    return a + b;
-};
-addExpr(2, 5);
-addExpr(2, 5, 8, 12)
+// // Arguments keyword        //Not actual, but good to know
+// const addExpr = function(a, b) {
+//     console.log(arguments)
+//     return a + b;
+// };
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12)
 
 // var addArrow = (a, b) => {
 //     console.log(arguments);
 //     return a + b;
 // };
 // addArrow(2, 5, 8);
+
+
+
+
+
+
+
+
+
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age)
+console.log(oldAge)
+
+
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend', friend);
+console.log('Me', me)
 
 
