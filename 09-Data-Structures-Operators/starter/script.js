e81fa19f99599e6623b2fe5f00ea39fcbe125cc5
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-07 03:30:24 +0900

    difference between spread and rest

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -37,6 +37,11 @@ const restaurant = {
   },
   orderPasta: function(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   }
 };
 
@@ -66,7 +71,24 @@ const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays)     // {thu: {…}, fri: {…}}
 
 // 2) Functions
+const add = function(...numbers) {
+  // console.log(numbers);
+  let sum = 0;
+  for(let i = 0; i < numbers.length; i++) 
+  // sum += numbers[i];
+  sum = sum + numbers[i];
+  console.log(sum)
+}
+add(2,3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
 
+const x = [23, 5, 7];
+add(...x);
+console.log(x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms')
 
 
 
