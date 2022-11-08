cbe65b18652bc9683cecf86cecc5132104a4ec27
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-09 03:59:53 +0900

    Enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,67 +1,88 @@
 'use strict';
-
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 12 + 12,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // openingHours: openingHours,   // BEFORE ES 6
+  openingHours,   // After ES 6
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  
 
-  order: function(starterIndex, mainIndex) {
+  // order: function(starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+
+  // orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+  //   console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+  //   );
+  // },
+  orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
     console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
-  orderPasta: function(ing1, ing2, ing3){
+  // orderPasta: function(ing1, ing2, ing3){
+  //   console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+  // },
+  orderPasta(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
   },
 
-  orderPizza: function(mainIngredient, ...otherIngredients) {
+  // orderPizza: function(mainIngredient, ...otherIngredients) {
+  //   console.log(mainIngredient);
+  //   console.log(otherIngredients);
+  // },
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   }
+  
 };
 
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menu)
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu)
 
-for(const food of menu) console.log(food);
+// for(const food of menu) console.log(food);
 
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1} : ${el}`)     
-  // 'i' is a number and 'el' is the name of food 
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1} : ${el}`)     
+//   // 'i' is a number and 'el' is the name of food 
 
 
 
-// for (const food of menu.entries()) {
-//   console.log(`${food[0] + 1} : ${food[1]}`)
-//   // console.log(food)
-}
-console.log([...menu.entries()])
+// // for (const food of menu.entries()) {
+// //   console.log(`${food[0] + 1} : ${food[1]}`)
+// //   // console.log(food)
+// }
+// console.log([...menu.entries()])
 
 
 
@@ -451,3 +472,13 @@ console.log([...menu.entries()])
 
 
 
+// const randomNum = [12, 21, 34, 55553, 53, 23, 47,]
+
+// const findNum = function(arr) {
+//   let max = arr[0];
+//   for( let i = 0; i < arr.length; i++) {
+//     let array = arr[i]
+//     if(array > max) max = array
+//   } console.log(max)
+// }
+// const check = findNum(randomNum)
