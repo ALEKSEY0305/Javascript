af8c7d0d21e8e051d854e908e23979751b3292f0
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-09 03:38:50 +0900

    for - of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -45,61 +45,84 @@ const restaurant = {
   }
 };
 
-const game = {
-  team1: 'Bayern Munich', team2: 'Borrussia Dortmund', players: [
-  [
-  'Neuer',
-        'Pavard',
-        'Martinez',
-        'Alaba',
-        'Davies',
-        'Kimmich',
-        'Goretzka',
-        'Coman',
-        'Muller',
-        'Gnarby',
-        'Lewandowski',
-  ], [
-        'Burki',
-        'Schulz',
-        'Hummels',
-        'Akanji',
-        'Hakimi',
-        'Weigl',
-        'Witsel',
-        'Hazard',
-        'Brandt',
-        'Sancho',
-        'Gotze',
-  ], ],
-    score: '4:0',
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-    'Hummels'],
-    date: 'Nov 9th, 2037',
-    odds: {
-      team1: 1.33,
-      x: 3.25,
-      team2: 6.5,
-  }, };
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu)
+
+for(const food of menu) console.log(food);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1} : ${el}`)     
+  // 'i' is a number and 'el' is the name of food 
+
+
+
+// for (const food of menu.entries()) {
+//   console.log(`${food[0] + 1} : ${food[1]}`)
+//   // console.log(food)
+}
+console.log([...menu.entries()])
+
+
+
+// const game = {
+//   team1: 'Bayern Munich', team2: 'Borrussia Dortmund', players: [
+//   [
+//   'Neuer',
+//         'Pavard',
+//         'Martinez',
+//         'Alaba',
+//         'Davies',
+//         'Kimmich',
+//         'Goretzka',
+//         'Coman',
+//         'Muller',
+//         'Gnarby',
+//         'Lewandowski',
+//   ], [
+//         'Burki',
+//         'Schulz',
+//         'Hummels',
+//         'Akanji',
+//         'Hakimi',
+//         'Weigl',
+//         'Witsel',
+//         'Hazard',
+//         'Brandt',
+//         'Sancho',
+//         'Gotze',
+//   ], ],
+//     score: '4:0',
+//     scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+//     'Hummels'],
+//     date: 'Nov 9th, 2037',
+//     odds: {
+//       team1: 1.33,
+//       x: 3.25,
+//       team2: 6.5,
+//   }, };
+
+
+
 
 
 // Challenge #1
-// 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// // 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
 
 
-//3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// //3.
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-//4. 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']
-console.log(players1Final);
+// //4. 
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']
+// console.log(players1Final);
 
 // 5. 
 // const team1 = game.odds.team1;
@@ -111,22 +134,22 @@ console.log(players1Final);
 // const team2 = game.odds.team2;
 // console.log(team2);
 
-const {odds: {team1, x: draw, team2}} = game;
-console.log(team1, draw, team2)
+// const {odds: {team1, x: draw, team2}} = game;
+// console.log(team1, draw, team2)
 
 
-// 6. 
-const printGoals = function(...goals){
-  console.log(goals)
-  console.log(`${goals.length} goals were scored`)
-}
-printGoals('Davies', 'Muller', 'Lewandowski','Kimmich');
-// printGoals(...game.scored)
+// // 6. 
+// const printGoals = function(...goals){
+//   console.log(goals)
+//   console.log(`${goals.length} goals were scored`)
+// }
+// printGoals('Davies', 'Muller', 'Lewandowski','Kimmich');
+// // printGoals(...game.scored)
 
 
-// 7. 
-const winner = game.odds.team1 <= game.odds.team2 ? `${game.team1} is likely to win` : `${game.team2} is likely to win`;
-console.log(winner)
+// // 7. 
+// const winner = game.odds.team1 <= game.odds.team2 ? `${game.team1} is likely to win` : `${game.team2} is likely to win`;
+// console.log(winner)
 
 
 
