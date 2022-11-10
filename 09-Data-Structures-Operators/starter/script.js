4156c8c9a9a005e3bd9b42bc6d4b9e2314e8c077
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-11 03:29:02 +0900

    maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -66,53 +66,113 @@ const restaurant = {
   
 };
 
-const ordersSet = new Set([
-  'Pasta', 
-  'Pizza', 
-  'Pizza', 
-  'Risotto', 
-  'Pasta', 
-  'Pizza',
-]);
 
-console.log(ordersSet);
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal'))
 
-console.log(new Set('Alex'));
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open', 11)
+.set('close', 23)
+.set(true, 'We are open')
+.set(false, 'We are closed')
 
-console.log(new Set());
+console.log(rest.get('name'))
+console.log(rest.get(true))
+console.log(rest.get(1))
 
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
 
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
+console.log(rest.has('categories'))
+rest.delete(2)
+// console.log(rest.clear())
+const arr = [1, 2]
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest)
+console.log(rest.size)
 
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-// ordersSet.delete('Risotto')
-console.log(ordersSet)
-
-for( const order of ordersSet) console.log(order)
-
-
-//EXAMPLE
-
-
-
-const staff = [
-  'waiter',
-  'chef',
-  'waiter',
-  'manager',
-  'chef',
-  'waiter',
-]
-
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(new Set(['waiter', 'chef', 'waiter','manager', 'chef', 'waiter',]).size)
+console.log(rest.get(arr));
 
 
-console.log(new Set('kimalekseyromanovich').size)
+
+
+// MAPS
+
+
+
+
+
+
+
+
+
+//SETS
+
+// const ordersSet = new Set([
+//   'Pasta', 
+//   'Pizza', 
+//   'Pizza', 
+//   'Risotto', 
+//   'Pasta', 
+//   'Pizza',
+// ]);
+
+// console.log(ordersSet);
+
+// console.log(new Set('Alex'));
+
+// console.log(new Set());
+
+
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// // ordersSet.delete('Risotto')
+// console.log(ordersSet)
+
+// for( const order of ordersSet) console.log(order)
+
+
+// //EXAMPLE
+
+
+
+// const staff = [
+//   'waiter',
+//   'chef',
+//   'waiter',
+//   'manager',
+//   'chef',
+//   'waiter',
+// ]
+
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(new Set(['waiter', 'chef', 'waiter','manager', 'chef', 'waiter',]).size)
+
+
+// console.log(new Set('kimalekseyromanovich').size)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // Property names
 // const properties = Object.keys(openingHours);   //Object.keys
 // console.log(properties)
