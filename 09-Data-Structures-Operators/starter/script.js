ab4fe56b844b07d087b9a3ca7685cf14b536df10
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-14 04:42:31 +0900

    challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
 
 // Data needed for first part of the section
 const openingHours = {
@@ -67,63 +65,111 @@ const restaurant = {
 };
 
 
+// Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// SPLIT and JOIN
-console.log('a+very+nice+string'.split('+'));
-console.log('Aleksey Kim'.split(' ')) // creates 1 array with 2 elements
+//   console.log(flights.split('+'))
 
-const [firstName, lastName] = 'Aleksey Kim'.split(' ')  // DIVIDES ARRAYS
+// const getCode = str => str.slice(0, 3).toUpperCase();
+
+//   for(const flight of flights.split('+')) {
+//     const [type, from, to, time] = flight.split(';');
+//     // const output = `${type.startsWith('_Delayed') ? '🆘' : ''}${type.replaceAll('_', ' ')} ${from.slice(0, 3)} ${to.slice(0, 3)} (${time.replace(':', 'h')})`;
+//     const output = `${type.startsWith('_Delayed') ? '🆘' : ''}${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(44);
+//     console.log(output)
+
+//   }
 
 
-const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ')
-console.log(newName)
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
 
 
-const capitalizedName = function(name) {
-  const names = name.split(' ')
-  const namesUpper = []
+document.querySelector('button').addEventListener('click',function(){
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows)
 
-  for(const n of names) {
-    namesUpper.push(n[0].toUpperCase() + n.slice(1))  // Adds the first letter with the rest of the name
-    // namesUpper.push(n.replace(n[0], n[0].toUpperCase()))
+
+  for(const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20, ' ')}${'✅'.repeat(i + 1)}`);
   }
-  console.log(namesUpper.join(' '))   // JOIN adds together the arrays
-}
-
-capitalizedName('jessica ann smith davis')
-capitalizedName('kim aleksey romanovich')
+})
 
 
-
-// PADDING
-const message = 'Go to gate 23!'
-console.log(message.padStart(25, '+').padEnd(30, '+'))
-console.log('Alex'.padStart(25, '+'))
-console.log('Alex'.padEnd(25, '+'))
-
-const maskCreditCard = function(number) {
-  const str = number + '';
-  const last = str.slice(-4)
-  return last.padStart(str.length, '*');
-}
-console.log(maskCreditCard(2314233))
-console.log(maskCreditCard(231421541242133))
-console.log(maskCreditCard('76823142154124213365'))
+// underscore_case
+//  first_name
+// Some_Variable 
+//   calculate_AGE
+// delayed_departure
 
 
+// underscore_case ====> underscoreCase
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
 
-// REPEAT
 
-const message2 = 'Bad weather... All Departures Delayed... ';
-console.log(message2.repeat(5))
+// // SPLIT and JOIN
+// console.log('a+very+nice+string'.split('+'));
+// console.log('Aleksey Kim'.split(' ')) // creates 1 array with 2 elements
 
-const planesInLine = function(n) {
-  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`)
-}
+// const [firstName, lastName] = 'Aleksey Kim'.split(' ')  // DIVIDES ARRAYS
 
-planesInLine(3)
-planesInLine(5)
-planesInLine(12)
+
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ')
+// console.log(newName)
+
+
+// const capitalizedName = function(name) {
+//   const names = name.split(' ')
+//   const namesUpper = []
+
+//   for(const n of names) {
+//     namesUpper.push(n[0].toUpperCase() + n.slice(1))  // Adds the first letter with the rest of the name
+//     // namesUpper.push(n.replace(n[0], n[0].toUpperCase()))
+//   }
+//   console.log(namesUpper.join(' '))   // JOIN adds together the arrays
+// }
+
+// capitalizedName('jessica ann smith davis')
+// capitalizedName('kim aleksey romanovich')
+
+
+
+// // PADDING
+// const message = 'Go to gate 23!'
+// console.log(message.padStart(25, '+').padEnd(30, '+'))
+// console.log('Alex'.padStart(25, '+'))
+// console.log('Alex'.padEnd(25, '+'))
+
+// const maskCreditCard = function(number) {
+//   const str = number + '';
+//   const last = str.slice(-4)
+//   return last.padStart(str.length, '*');
+// }
+// console.log(maskCreditCard(2314233))
+// console.log(maskCreditCard(231421541242133))
+// console.log(maskCreditCard('76823142154124213365'))
+
+
+
+// // REPEAT
+
+// const message2 = 'Bad weather... All Departures Delayed... ';
+// console.log(message2.repeat(5))
+
+// const planesInLine = function(n) {
+//   console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`)
+// }
+
+// planesInLine(3)
+// planesInLine(5)
+// planesInLine(12)
 
 // WORKING WITH STRING - PART 2
 
