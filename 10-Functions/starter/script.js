696575e36387c40954eca0503c3b0506f71539c4
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-18 03:39:12 +0900

    IIFE update

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -307,28 +307,29 @@
 // [2, 5, 3, 9, 6, 1]
 
 
-const runOnce = function() {
-        console.log('This will never run again')
-}
-runOnce();
+// const runOnce = function() {
+//         console.log('This will never run again')
+// }
+// runOnce();
 
 
-// IIFE
-(function() {
-        console.log('This will never run again')
-        const isPrivate = 23;
+// // IIFE
+// (function() {
+//         console.log('This will never run again')
+//         const isPrivate = 23;
         
-})();   
-// console.log(isPrivate);
+// })();   
+// // console.log(isPrivate);
 
 
-(() =>  console.log('This will ALSO never run again'))();
+// (() =>  console.log('This will ALSO never run again'))();
 
-// WHY NEED IIFE?
+// // WHY NEED IIFE?
 
-{
-        const isPrivate = 23;
-        var notPrivate = 46
-}
-// console.log(isPrivate);
-console.log(notPrivate)
+// {
+//         const isPrivate = 23;
+//         var notPrivate = 46
+        
+// }
+// // console.log(isPrivate);
+// console.log(notPrivate)                 // VAr can access data from outside the scope
