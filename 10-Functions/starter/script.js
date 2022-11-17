1a43584437c2a51d873c6559457fecda38ed6f87
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-18 04:48:23 +0900

    more about closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -339,29 +339,95 @@
 
 
 
-// CLOSURES
+// // CLOSURES
 
-const secureBooking = function() {
-        let passengerCount = 0;
+// const secureBooking = function() {
+//         let passengerCount = 0;
 
 
-        return function() {
-                passengerCount++;
-                console.log(`${passengerCount} passengers`);
+//         return function() {
+//                 passengerCount++;
+//                 console.log(`${passengerCount} passengers`);
+//         }
+// }
+
+// const booker = secureBooking()
+// booker();
+// booker();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker)
+
+
+let f;
+
+const g = function () {
+        const a = 23;
+        f = function() {
+                console.log(a * 2)
+        }
+}
+const h = function() {
+        const b = 777;
+        f = function() {
+                console.log(b * 2);
         }
 }
 
-const booker = secureBooking()
-booker();
-booker();
-booker();
-booker();
-booker();
-
-console.dir(booker)
-
+g()
+f()
+console.dir(f)
+// reassigning f function
+h()
+f()
+console.dir(f)
 
 
+
+// example 2
+const boardPassengers = function(n, wait) {
+        const perGroup = n / 3;
+
+        setTimeout(function(){
+                console.log(`We are now boarding all ${n} passengers`)
+                console.log(`There are 3 groups, each with ${perGroup} passengers`);
+        }, wait * 1000)
+
+        console.log(`Will start boarding in ${wait} seconds`);
+}
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+setTimeout(function(){
+        console.log(`You are amazing!`)
+}, 1000)
 
 //MISTAKE CLOSURE
 
