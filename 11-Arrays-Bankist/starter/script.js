f3ed82c730e9f201117208631b82206d547ea68c
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-20 16:54:28 +0900

    bankist movement container, beforeend, innerHTML

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+const displayMovements = function(movements){
+  movements.forEach(function( mov, i){
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+      </div>`
+      containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+displayMovements(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -184,28 +199,28 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 
-currencies.forEach(function(value, key, map) {
-  console.log(`${key}: ${value}`);
-});
+// currencies.forEach(function(value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
 
-  //USD: United States dollar
-  //EUR: Euro
-  //GBP: Pound sterling
+//   //USD: United States dollar
+//   //EUR: Euro
+//   //GBP: Pound sterling
 
-// SET
+// // SET
 
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique)
-currenciesUnique.forEach(function(value, _ , map) {
-  console.log(`${value}: ${value}`);  
-  //USD: USD
-  //GBP: GBP
-  //EUR: EUR
-});
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique)
+// currenciesUnique.forEach(function(value, _ , map) {
+//   console.log(`${value}: ${value}`);  
+//   //USD: USD
+//   //GBP: GBP
+//   //EUR: EUR
+// });
