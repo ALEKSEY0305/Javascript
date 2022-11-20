6708acc44c90bfed739fbd692de9efa8c0971486
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-11-21 00:43:07 +0900

    map, filter, reduce

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -92,32 +92,32 @@ displayMovements(account1.movements)
 //     // });
   
 // }
-// checkDogs()
-const checkDogs = function(dogsJulia, dogsKate) {
-  // const correctedDogsJulia = dogsJulia.slice(1,3);   is okay, but slice is also good
-  const correctedDogsJulia = dogsJulia.slice();
-  correctedDogsJulia.splice(0, 1);
-  correctedDogsJulia.splice(-2);
-  console.log(correctedDogsJulia)
-  const allDogs = correctedDogsJulia.concat(dogsKate)
-  // const allDogs= [...correctedDogsJulia, ...dogsKate]
-  console.log(allDogs)
-  allDogs.forEach(function(dog, i) {
-    if (dog >= 3) {
-      console.log(`Dog number ${i + 1} is an adult 🦮, and is ${dog} year(s) old`)
-    } else {
-      console.log(`Dog number ${i + 1} is a puppy 🐶, and is ${dog} year(s) old`)
-    }
-  })
-};
-
-
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
-
-// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])
-
 
 //CHALLENGE 1
+
+// checkDogs()
+// const checkDogs = function(dogsJulia, dogsKate) {
+//   // const correctedDogsJulia = dogsJulia.slice(1,3);   is okay, but slice is also good
+//   const correctedDogsJulia = dogsJulia.slice();
+//   correctedDogsJulia.splice(0, 1);
+//   correctedDogsJulia.splice(-2);
+//   console.log(correctedDogsJulia)
+//   const allDogs = correctedDogsJulia.concat(dogsKate)
+//   // const allDogs= [...correctedDogsJulia, ...dogsKate]
+//   console.log(allDogs)
+//   allDogs.forEach(function(dog, i) {
+//     if (dog >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult 🦮, and is ${dog} year(s) old`)
+//     } else {
+//       console.log(`Dog number ${i + 1} is a puppy 🐶, and is ${dog} year(s) old`)
+//     }
+//   })
+// };
+
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -267,3 +267,10 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
 //   //GBP: GBP
 //   //EUR: EUR
 // });
+
+
+
+
+
+
+// MAP, FILTER, REDUCE
