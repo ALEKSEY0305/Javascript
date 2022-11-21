54c3e00e9c897ad76aa55ffdb70f8d6fb4a7e77f
Alex <112188056+ALEKSEY0305@users.noreply.github.com>
2022-11-22 03:51:42 +0900

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -78,6 +78,31 @@ displayMovements(account1.movements)
 // console.log(containerMovements.innerHTML)
 
 
+
+
+
+
+// const username = 'Steven Thomas Williams'; // stw
+
+  
+// const createUsernames = function(accs) {
+//   accs.forEach(function(acc) {
+//     acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join(' ');
+//   });
+// }
+
+
+// createUsernames(accounts);
+// console.log(...accounts)
+
+
+
+
+
+
+
+
+
 // const checkDogs = function() {
 //   const dogsJulia = [3, 5, 2, 12, 7];
 //   const dogsKate = [4, 1, 15, 8, 3];
@@ -273,3 +298,35 @@ displayMovements(account1.movements)
 
 
 // MAP, FILTER, REDUCE
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+
+//the map method
+// const eurToUsd = 1.1;
+
+// const movementssUSD = movements.map(function(mov) {
+//   return mov * eurToUsd;
+// })
+
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+// console.log(movements)
+// console.log(movementsUSD)
+// console.log(movementssUSD)
+
+// const movementsUSDfor = [];
+// for(const mov of movements) movementsUSDfor.push(mov * eurToUsd)
+// console.log(movementsUSDfor);
+
+// const movementsDescriptions = movements.map((mov, i) => {    
+//   `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+//   if( mov > 0) {     // in forEach, the 1st el is the current el, the 2nd is the index 
+//     return `Movement ${i + 1}: You deposited ${mov}`;
+//   } else {
+//     return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}` // abs = absolute
+//   }
+// });
+// console.log(movementsDescriptions)
+
+
+// COMPUTING USERNAMES
