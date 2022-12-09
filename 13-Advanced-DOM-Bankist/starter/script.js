e034b69dfc4959fa87afed4166c0ce2636b23a67
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-12-10 02:32:03 +0900

    bubbling, capturing events, this, stop propagation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -193,26 +193,81 @@ btnScrollTo.addEventListener('click', () => section1.scrollIntoView({behavior: '
 // });
 
 
+
+
+
+
+
+
+
+
+
+//------------------------------------------------
 //    EVENTS
 
 //New Way
-const h1 = document.querySelector('h1')
+// const h1 = document.querySelector('h1')
 
 
-const alertH1 = function(e) {
-  alert('addEventListener: You are reading the header!')
+// const alertH1 = function(e) {
+//   alert('addEventListener: You are reading the header!')
 
-  h1.removeEventListener('mouseenter', alertH1);
+//   h1.removeEventListener('mouseenter', alertH1);
 
-}
+// }
 
-h1.addEventListener('mouseenter', alertH1);
+// h1.addEventListener('mouseenter', alertH1);
 
-
-  
   // setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 //Old way
 // h1.onmouseenter = function(e) {
 //   alert('onmouseenter: You are reading the header!')
 // };
 
+
+
+
+
+
+
+
+// rgb(255,255,255)
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + 1);
+
+const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+
+console.log(randomColor(0, 255))
+console.log(randomColor(0, 255))
+console.log(randomColor(0, 255)) 
+
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+  // console.log('Link');
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target, e.currentTarget);
+
+
+
+  // stop propagation
+  // e.stopPropagation()
+});
+
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  // console.log('Link');
+  this.style.backgroundColor = randomColor()
+  console.log('con', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    // console.log('Link')
+    this.style.backgroundColor = randomColor();
+    console.log('nav', e.target, e.currentTarget);
+  },
+  true
+);
