2de6cb0bb1a3c82f5f3efcc98b1b7f7b56c4127c
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-12-13 03:48:08 +0900

    another intersection API example

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -159,12 +159,15 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 
 
+// Intersection observer API 
 
 const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if(!entry.isIntersecting) nav.classList.add('sticky');
   else  nav.classList.remove('sticky');
 
@@ -173,9 +176,37 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: '-90px'
+  rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+
+
+// Reveal Sections
+const allSections = document.querySelectorAll('.section')
+
+const revealSection = function(entries, observer) {
+  const[entry] = entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+}); 
+
+
 
 
 /////////////////////////////////////////////////
