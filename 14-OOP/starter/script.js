6e7311bdd5ad2c4b8c7673fb9f873c316cdb98cd
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-12-16 04:16:39 +0900

    prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -26,3 +26,41 @@ console.log(jack)
 
 console.log(alex instanceof Person)
 // console.log(jay instanceof Person)
+
+
+
+
+// Prototypes
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function() {
+    console.log(2022 - this.birthYear)
+};
+
+alex.calcAge()
+matilda.calcAge()
+jack.calcAge()
+console.log(alex)
+console.log(jack)
+console.log(matilda)
+
+console.log(alex.__proto__)
+console.log(alex.__proto__ === Person.prototype)
+
+
+console.log(Person.prototype.isPrototypeOf(alex))
+console.log(Person.prototype.isPrototypeOf(matilda))
+console.log(Person.prototype.isPrototypeOf(Person))
+
+
+// const me = {
+//     name: 'Alex',
+//     age: 18,
+// }
+// console.log(me)
+
+// let newMe = me
+
+// newMe.age = 25
+
+// console.log(me)
