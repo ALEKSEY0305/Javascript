249838886a8d06c5e11c99eb3f0ba0b729394f9a
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-01-10 03:43:07 +0900

    added leaflet library and changed the map theme

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -15,9 +15,24 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
-        console.log(`https://www.google.com/maps/dir/${latitude}, ${longitude}`) 
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/dir/${latitude}, ${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13); // number is the x of zoom
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        //fr/ hot is a new theme
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
