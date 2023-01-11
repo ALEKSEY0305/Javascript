01a29fc64cd5c351f2bc44f4299fcbd22816813a
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-01-12 05:10:28 +0900

    promise

---
+++ 15-Mapty/starter/mdb.js
@@ -0,0 +1,38 @@
+
+
+// // async 
+// async function f1() {
+//     const res = await fetch(console.log(1))
+//     return res.text()
+// }
+
+// function f2() {
+//     console.log(2)
+// }
+
+// function f3() {
+//     console.log(3)
+// }
+
+// f1();
+// f2();
+// f3();
+
+let a = 7;
+
+
+
+// console.log(a);
+
+// let b = new Promise(function(resolve, reject) { // promise needs func resolve, reject
+//     setTimeout(() => {                          // and .then(funct)
+//         resolve(a = 99);                        // catch
+//     },2000) 
+// });
+
+// b.then(function() {
+//     console.log(a);
+// })
+
+// console.log(a)
+
